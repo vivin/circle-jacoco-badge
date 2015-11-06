2ad87a28457d95795df8eda7de58ff63882eb616
Vivin Paliath <vivin.paliath@gmail.com>
2015-11-06 14:25:41 -0700

    Adding support for branch coverage and cyclomatic complexity

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const CIRCLE_CI_URL = "https://circleci.com/api/v1/project";
 const AUTHOR = "author";
 const PROJECT = "project";
 const CIRCLE_TOKEN = "circle-token";
+const TYPE = "type";
 
 var params = null;
 
@@ -43,7 +44,7 @@ dispatcher.onGet("/badge", function(httpRequest, httpResponse) {
             json: true
         }, function(error, response, builds) {
             if(!error && response.statusCode === 200) {
-                getBadge(builds[0], function(badge) {
+                getBadge(params[TYPE] || "line", builds[0], function(badge) {
                     httpResponse.writeHead(200, {
                         'Content-Type': 'image/png',
                         'Cache-Control': 'no-cache',
@@ -96,19 +97,21 @@ function getReport(build, callback) {
     });
 }
 
-function getBadge(build, callback) {
+function getBadge(type, build, callback) {
     processArtifact(/jacocoTestReport\.xml$/, build, function(artifact) {
         request({
             url: artifact.url + "?" + CIRCLE_TOKEN + "=" + params[CIRCLE_TOKEN]
         }, function(error, response, body) {
             xml2js.parseString(body, function(err, result) {
+                console.log(JSON.stringify(result.report.counter));
+                var counters = {};
                 result.report.counter.forEach(function(element) {
-                    if(element.$.type === "INSTRUCTION") {
-                        callback({
-                            image: generateBadge(element.$, false),
-                            stopTime: build.stop_time
-                        });
-                    }
+                    counters[element.$.type] = element.$;
+                });
+
+                callback({
+                    image: generateBadge(type, counters, false),
+                    stopTime: build.stop_time
                 });
             });
         });
@@ -131,8 +134,13 @@ function processArtifact(artifactPattern, build, callback) {
     });
 }
 
-function generateBadge(coverage, error) {
-    var width = 90;
+function generateBadge(type, counters, error) {
+    type = (type === "line") ? "INSTRUCTION" : (type === "branch") ? "BRANCH" : (type === "complexity") ? "COMPLEXITY": "INSTRUCTION";
+
+    var diff = (type === "INSTRUCTION" || type == "BRANCH") ? 0 : (type === "COMPLEXITY") ? 10 : 0;
+    var textX = (type === "INSTRUCTION") ? 3.5 : (type === "BRANCH") ? 10.5 : (type === "COMPLEXITY") ? 4.5 : 3.5;
+
+    var width = 90 + diff;
     var height = 18;
     var radius = 10;
 
@@ -141,18 +149,44 @@ function generateBadge(coverage, error) {
 
     context.antialias = "subpixel";
 
+    var metric = error ? "!!!" : "";
+    var coverageType = "";
+    var color = error ? "red" : "green";
     if(!error) {
-        var covered = parseInt(coverage.covered);
-        var missed = parseInt(coverage.missed);
+        if(type === "INSTRUCTION" || type === "BRANCH") {
+            coverageType = (type === "INSTRUCTION") ? "coverage" : "branch";
+            var covered = parseInt(counters[type].covered, 10);
+            var missed = parseInt(counters[type].missed, 10);
 
-        var percentage = Math.round((covered / (covered + missed)) * 100);
-    }
+            metric = Math.round((covered / (covered + missed)) * 100) + "%";
 
-    var color = "green";
-    if(color >= 70 && color < 85) {
-        color = "orange";
-    } else if(color < 70 || error) {
-        color = "red"
+            if(metric >= 70 && metric < 85) {
+                color = "orange";
+            } else if(metric >= 60 && metric < 70) {
+                color = "darkorange";
+            } else if (metric < 60) {
+                color = "darkred";
+            }
+        } else if(type === "COMPLEXITY") {
+            coverageType = "complexity";
+            var totalComplexity = parseInt(counters[type].missed, 10) + parseInt(counters[type].covered, 10);
+            metric = (totalComplexity / parseInt(counters.METHOD.covered, 10));
+            if(metric < 10) {
+                metric = metric.toFixed(2);
+            } else if(metric >= 10 && metric < 100) {
+                metric = metric.toFixed(1);
+            } else {
+                metric = metric.toFixed(0);
+            }
+
+            if(metric > 10 && metric <= 20) {
+                color = "orange";
+            } else if(metric > 20 && metric <= 40) {
+                color = "darkorange";
+            } else if(metric > 40) {
+                color = "red";
+            }
+        }
     }
 
     context.beginPath();
@@ -162,12 +196,15 @@ function generateBadge(coverage, error) {
     context.lineJoin = "round";
     context.lineWidth = radius;
 
-    context.strokeRect(radius / 2, radius / 2, width - radius, height - radius);
-    context.fillRect(radius / 2, radius / 2, width - radius, height - radius);
+    context.strokeRect(radius / 2, radius / 2, (width - 30 - diff) - radius, height - radius);
+    context.fillRect(radius / 2, radius / 2, (width - 30 - diff) - radius, height - radius);
 
     context.lineJoin = "miter";
-    context.moveTo(50 + (radius / 2), 0);
-    context.lineTo(50 + (radius / 2), height);
+    context.moveTo(50 + diff + (radius / 2), 0);
+    context.lineTo(50 + diff + (radius / 2), height);
+    context.stroke();
+    context.moveTo(50 + diff, 0);
+    context.lineTo(50 + diff, height);
     context.stroke();
     context.closePath();
 
@@ -176,12 +213,12 @@ function generateBadge(coverage, error) {
     context.strokeStyle = color;
     context.fillStyle = color;
 
-    context.strokeRect(60 + (radius / 2), radius/2, width - radius - 60, height - radius);
-    context.fillRect(60 + (radius / 2), radius / 2, width - radius - 60, height-radius);
+    context.strokeRect(60 + diff + (radius / 2), radius/2, width - radius - (60 + diff), height - radius);
+    context.fillRect(60 + diff + (radius / 2), radius / 2, width - radius - (60 + diff), height - radius);
 
     context.lineJoin = "miter";
-    context.moveTo(60 + (radius / 2), 0);
-    context.lineTo(60 + (radius / 2), height);
+    context.moveTo(60 + diff + (radius / 2), 0);
+    context.lineTo(60 + diff + (radius / 2), height);
     context.stroke();
     context.closePath();
 
@@ -191,14 +228,14 @@ function generateBadge(coverage, error) {
     context.lineJoin = "miter";
 
     context.font = "600 7.5pt sans-serif";
-    context.fillText("coverage", 3.5, 12);
+    context.fillText(coverageType, textX, 12);
 
     if(!error) {
         context.font = "600 7.5pt sans-serif";
-        context.fillText(percentage + "%", 62, 12.5);
+        context.fillText(metric, 62 + ((10 - diff) / 10) + diff, 12.5);
     } else {
         context.font = "bold 7.5pt sans-serif";
-        context.fillText("??%", 62, 12.5);
+        context.fillText("??%", 62 + ((10 - diff) / 10) + diff, 12.5);
     }
 
     return canvas.toBuffer();
